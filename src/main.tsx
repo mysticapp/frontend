5bec7658532d12d304334862eb72c24797c9d7b7
Kamil Grabara <mafineeekdev@gmail.com>
2022-11-28 22:02:48 +0100

    simple login site

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
+import Login from "./pages/auth/Login";
+import { ThemeProvider } from "./ThemeProvider";
 
 const Mystic = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<App/>} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<Mystic/>);
+ReactDOM.createRoot(document.getElementById("root")!).render(<Mystic />);
