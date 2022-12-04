d47817f1245c1d16e30636aaca7555ef9e197616
Kamil Grabara <mafineeekdev@gmail.com>
2022-12-04 14:48:03 +0100

    SCROLL AREA DONE

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -1,43 +1,46 @@
-import { Input, Group, Stack } from "@mantine/core";
+import { Input, Group, Stack, ScrollArea } from "@mantine/core";
 import Message from "./Chat/MessageComponent";
 import ChannelTopBar from "./GuildComponents/ChannelName";
 
 export default function ChatComponent() {
   return (
     <>
-      <Stack sx={{height:'100%', width: '100%'}}>
-    <ChannelTopBar/>
-        <Stack>
+      <Stack sx={{ height: "100%", width: "100%" }}>
+        <ChannelTopBar />
+        <ScrollArea sx={{height: '87vh'}}>
+          <Stack sx={{height: '100%'}}>
           <Message
-            author={{
-              _id: "123",
-              username: "mafineeek",
-              avatar:
-                "https://cdn.discordapp.com/avatars/854342480019587133/a_d195a0d4f1f1ebd6371abef25ceb845d.gif?size=4096",
-            }}
-            message={{ content: "test", createdAt: new Date() }}
-          />
+              author={{
+                _id: "123",
+                username: "mafineeek",
+                avatar:
+                  "https://cdn.discordapp.com/avatars/854342480019587133/a_d195a0d4f1f1ebd6371abef25ceb845d.gif?size=4096",
+              }}
+              message={{ content: "test", createdAt: new Date() }}
+            />
           <Message
-            author={{
-              _id: "123",
-              username: "mafineeek",
-              avatar:
-                "https://cdn.discordapp.com/avatars/854342480019587133/a_d195a0d4f1f1ebd6371abef25ceb845d.gif?size=4096",
-            }}
-            message={{ content: "test", createdAt: new Date() }}
-          />
+              author={{
+                _id: "123",
+                username: "mafineeek",
+                avatar:
+                  "https://cdn.discordapp.com/avatars/854342480019587133/a_d195a0d4f1f1ebd6371abef25ceb845d.gif?size=4096",
+              }}
+              message={{ content: "test", createdAt: new Date() }}
+            />
           <Message
-            author={{
-              _id: "123",
-              username: "mafineeek",
-              avatar:
-                "https://cdn.discordapp.com/avatars/854342480019587133/a_d195a0d4f1f1ebd6371abef25ceb845d.gif?size=4096",
-            }}
-            message={{ content: "test", createdAt: new Date() }}
-          />
-        </Stack>
-        <Group align={"center"}>
-          <Input.Wrapper w={"100%"}>
+              author={{
+                _id: "123",
+                username: "mafineeek",
+                avatar:
+                  "https://cdn.discordapp.com/avatars/854342480019587133/a_d195a0d4f1f1ebd6371abef25ceb845d.gif?size=4096",
+              }}
+              message={{ content: "test", createdAt: new Date() }}
+            />
+          </Stack>
+        </ScrollArea>
+      </Stack>
+        <Group sx={{ position: "absolute", bottom: 15, width: "80%" }} align={"center"}>
+          <Input.Wrapper sx={{width: "100%"}}>
             <Input
               type={"text"}
               placeholder={"Type your message here..."}
@@ -46,7 +49,6 @@ export default function ChatComponent() {
             ></Input>
           </Input.Wrapper>
         </Group>
-      </Stack>
     </>
   );
 }
